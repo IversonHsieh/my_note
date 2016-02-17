87d81030dc9c08cbf67dda0388b9775c89a71749
Iverson Hsieh <crttcrttcrtt@gmail.com>
2016-02-17 11:26:53 +0800

    Add nodejs - first version

---
+++ nodejs/helloworld.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+//console.log(global);
+console.log("global.process.version=", global.process.version);
+console.log("Buffer=", Buffer);
+console.log("====================================================");
+console.log("Buffer.isEncoding=",Buffer.isEncoding);
+console.log("====================================================");
+console.log("Buffer.poolSize=",Buffer.poolSize);
